119185d04097b32e912f15ea428310a1d3f1fcca
Kurian Vithayathil <kvithayathil@users.noreply.github.com>
2017-12-21 14:44:12 -0500

    Initial rewrite of main activity with fragment and backstack updates

---
+++ app/src/main/java/com/example/kurianvithayathil/keddit/kedditbysteps/news/NewsFragment.kt
@@ -0,0 +1,8 @@
+package com.example.kurianvithayathil.keddit.kedditbysteps.news
+
+import android.support.v4.app.Fragment
+
+/**
+ * Created by kurianvithayathil on 12/21/17.
+ */
+class NewsFragment : Fragment()
