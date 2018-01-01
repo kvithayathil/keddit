1675e32129f05ec6fc3b5f34734e1dd8a2c76875
Kurian Vithayathil <k.vithayathil@gmail.com>
2018-01-01 15:05:40 -0500

    - Extension functions and android extensions on list

--- app/src/main/java/com/example/kurianvithayathil/keddit/kedditbysteps/news/NewsFragment.kt
+++ app/src/main/java/com/example/kurianvithayathil/keddit/kedditbysteps/news/NewsFragment.kt
@@ -3,24 +3,24 @@ package com.example.kurianvithayathil.keddit.kedditbysteps.news
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.kurianvithayathil.keddit.kedditbysteps.R
 import com.example.kurianvithayathil.keddit.kedditbysteps.commons.inflate
+import kotlinx.android.synthetic.main.news_fragment.*
 
 /**
  * Created by kurianvithayathil on 12/21/17.
  */
 class NewsFragment : Fragment() {
-    private var newsList: RecyclerView? = null
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = container?.inflate(R.layout.news_fragment)
-        newsList = view?.findViewById(R.id.news_list)
-        newsList?.setHasFixedSize(true)
-        newsList?.layoutManager = LinearLayoutManager(context)
-        return view
+        return container?.inflate(R.layout.news_fragment)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        news_list.setHasFixedSize(true)
+        news_list.layoutManager = LinearLayoutManager(context)
     }
 }
